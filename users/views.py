e5804cd36e06fce6b7278fae85c1dcda11500a91
Tomas Pecukevicius <pecukevicius@gmail.com>
2024-02-19 11:33:48 +0000

    redeploying to heroku with static files

---
+++ users/views.py
@@ -0,0 +1,27 @@
+from django.contrib.auth import authenticate, login, logout as auth_logout
+from django.contrib.auth import authenticate, login, logout as auth_logout
+from django.contrib.auth.decorators import login_required
+
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
+
+# User Login
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have successfully logged in.')
+            return redirect('home')  # Redirect to
+            # the logged-in user's profile
+        else:
+            messages.error(request,
+                           'Invalid login credentials. Please try again.')
+    return render(request, 'user_login.html')
+
+
+# User Registration
+
