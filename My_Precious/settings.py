273e9150c9bfcb5b7c552a73e949cbfa2ae5c8fd
Tomas Pecukevicius <pecukevicius@gmail.com>
2024-02-19 12:16:19 +0000

    created login template

--- My_Precious/settings.py
+++ My_Precious/settings.py
@@ -47,6 +47,7 @@ CSRF_TRUSTED_ORIGINS = ['https://*.herokuapp.com']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,7 +124,7 @@ STATIC_URL = '/static/'
 
 # The absolute path to the directory where collectstatic will collect static
 # files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
